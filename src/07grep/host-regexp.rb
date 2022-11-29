9b21cd31cf97ae26b27fcad15e723166834f135d
mimaki <hiroshi.mimaki@gmail.com>
2022-11-29 17:21:26 +0900

    Fix regexp build on MinGW.

--- src/07grep/host-regexp.rb
+++ src/07grep/host-regexp.rb
@@ -4,7 +4,7 @@ MRuby::Build.new do |conf|
   # include the default GEMs
   conf.gembox 'default'
   # Use mrbgems
-  conf.gem :git => 'https://github.com/mattn/mruby-onig-regexp.git'
+  conf.gem :git => 'https://github.com/iij/mruby-regexp-prce.git'
 
   # C compiler settings
   conf.cc.defines = %w(MRB_UTF8_STRING)
